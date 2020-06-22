c3e2bf004476cb55a3d67c0f40319d203cda4058
Jwilson1172 <jw59615@gmail.com>
2020-06-22 11:08:03 -0400

    init commit

---
+++ test_nativebayes.py
@@ -0,0 +1,22 @@
+import unittest
+
+
+class TestNB(unittest.TestCase):
+    # testing the fit method of NBC for accuracy
+    # and make sure that it is behaving as exspected
+    def test_fit(self):
+        """Doc String Placeholder
+        """
+        return
+
+    def test_train(self):
+        """Doc String Placeholder
+        """
+        return
+
+    def test_score(self):
+        """Doc String Placeholder
+        """
+        return
+
+    pass
