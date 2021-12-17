f1f76374055b4e4456511d530465269058ceccbc
JWilson <jw59615@gmail.com>
2021-12-17 06:00:24 -0500

    adding bayes therory and testing notebook

--- naivebayes.py
+++ naivebayes.py
@@ -1,7 +1,73 @@
-import math
-import numpy as np
-import pandas as pd
 from sklearn.base import BaseEstimator
+from pandas import DataFrame, Series
+from collections import Counter
+
+
+class BayesTherory:
+    def __init__(self):
+        pass
+
+    def prob_given_label(self, y, cond_y):
+        # copy input to avoid changing original
+        y_cp = y[:]
+        # start count
+        i = 0
+        # save len of original y
+        bottom = len(y_cp)
+        # check if last element is None (breakpoint case)
+        if y_cp[-1] is not None:
+            y_cp.append(None)
+
+        # count target observations
+        while True:
+            if y_cp[0] == cond_y:
+                i += 1
+            # break on None item
+            elif y_cp[0] is None:
+                break
+            y_cp.pop(0)
+        # return ratio of labels to len
+        return i / bottom
+
+    def prob_feature_given_label(self, X: DataFrame, y: Series,
+                                 cond_y) -> dict:
+        # function that takes an input array of features and returns the
+        # marginal probability of a feature effecting the class label
+        ret = {}
+        y = y.copy().to_list()
+        for feature in X.columns:
+            d = self.__conditional_counter(X[feature].to_list(), y, cond_y)
+            d = self.__calc_f_proba(d, y, cond_y)
+            ret[feature] = d
+        return ret
+
+    def __conditional_counter(self, x: list, y: list, cond_y) -> dict:
+        # function that counts unique values in X that meat cond_y value in y
+        # for instance:
+        # given
+        # x,y = ([1,0,1,0,1],['y','n','y','n','y'])
+        # and
+        # cond_y = 'y'
+        # this function will look through x where y == 'y' and count
+        # the unique x values then return a dictionary with those keys and
+        # and the number of observations
+        # __conditional_counter(x,y,cond_y)
+        # output -> {1: 3}
+
+        d = {}
+        for k, v in zip(x, y):
+            if v == cond_y:
+                if k in d:
+                    d[k] += 1
+                else:
+                    d[k] = 1
+        return d
+
+    def __calc_f_proba(self, d: dict, y: list, cond_y) -> dict:
+        # return a dictionary of each feature value as keys and the
+        # probability of that feature being in the target class
+        n = y.count(cond_y)
+        return {k: v / n for k, v in zip(d.keys(), d.values())}
 
 
 class NaiveBayesClassifier(BaseEstimator):
@@ -15,7 +81,4 @@ class NaiveBayesClassifier(BaseEstimator):
         pass
 
     def predict_proba(self, X, use_labels=False):
-        pass
-
-    def score(self, y_true, y_pred):
         pass
