7ec1f99b21c5db42904ba3e6dcd47b665b9ebf70
JWilson <jw59615@gmail.com>
2021-12-17 19:24:26 -0500

    adding src as a package for local notebook execution work around

---
+++ modules/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="NaiveBayes",
+    version="0.0.1",
+    description="A work around for working from a subdir in a notebook",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.8',
+)
